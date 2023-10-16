8168f2c9e14a3825f60cb17e3c7e81c17487c1da
Johan Hennius <johan.bh19@gmail.com>
2023-10-16 11:20:32 +0000

    increment right and wrong answer score implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,8 +50,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("That is correct.");
+        incrementScore();
     } else {
         alert(`${userAnswer} is incorrect. The correct answer was ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -77,12 +79,23 @@ function calculateCorrectAnswer() {
 
 }
 
+
+/**gets the current score from DOM and increments it by 1
+ * 
+*/
 function incrementScore() {
     
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
+/**get the current tally of incorrect answers from the DOM and increments it by 1 */
 function incrementWrongAnswer() {
     
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+
 }
 
 function displayAdditionQuestion(operand1, operand2) {
